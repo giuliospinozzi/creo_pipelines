5e1abb6a88ae06c28052d2c4a79e6e72428402f9
Valentina Tini <valy.tini@hotmail.it>
2018-10-10 10:43:24 +0200

    Fixed a bug in the gene ontology analysis

--- pipelines/GO_pathway.R
+++ pipelines/GO_pathway.R
@@ -27,6 +27,7 @@ if (dea == "cummeRbund") {
 colnames(table1)=c("Gene","logFC","FDR")
 table1=table1[table1$logFC!=Inf&table1$logFC!=-Inf,]
 
+library(rlist)
 library(clusterProfiler)
 library(dplyr)
 library(org.Hs.eg.db)
@@ -43,22 +44,33 @@ GO_CC=enrichGO(table1[abs(table1$logFC)>1.5 & table1$FDR<0.05,"entrez"], ont="CC
                pAdjustMethod = "fdr", universe = table1$entrez, OrgDb = org.Hs.eg.db, readable = T)
 GO_MF=enrichGO(table1[abs(table1$logFC)>1.5 & table1$FDR<0.05,"entrez"], ont="MF", pvalueCutoff= 0.05, 
                pAdjustMethod = "fdr", universe = table1$entrez, OrgDb = org.Hs.eg.db, readable = T)
-GO_BP@result$GO_domain="biological_process"
-GO_CC@result$GO_domain="cellular_component"
-GO_MF@result$GO_domain="molecular_function"
+res_all=list()
+if (nrow(GO_BP@result)!=0) {
+  GO_BP@result$GO_domain="biological_process"
+  res_all=list.append(res_all,GO_BP@result)
+  }
+if (nrow(GO_CC@result)!=0) {
+  GO_CC@result$GO_domain="cellular_component"
+  res_all=list.append(res_all,GO_CC@result)
+  }
+if (nrow(GO_MF@result)!=0) {
+  GO_MF@result$GO_domain="molecular_function"
+  res_all=list.append(res_all,GO_MF@result)
+  }
 GO=rbind(GO_BP@result,GO_CC@result,GO_MF@result)
 write.csv(GO,paste0(output_dir,"/Gene_ontology/GO_fc1.5_pv0.05.csv"),row.names = F)
 
 # treemap
 library(treemap)
-tree_BP=GO_BP@result[,c(2,9)]
-tree_BP$namespace_1003="biological_process"
-tree_CC=GO_CC@result[,c(2,9)]
-tree_CC$namespace_1003="cellular_component"
-tree_MF=GO_MF@result[,c(2,9)]
-tree_MF$namespace_1003="molecular_function"
-all=merge(tree_BP,tree_CC,all.x=T,all.y=T)
-all=merge(tree_MF,all,all.x=T,all.y=T)
+tree_all=list()
+for (i in 1:length(res_all)) {
+  tree=res_all[[i]][,c(2,9)]
+  tree$namespace_1003=res_all[[i]][1,10]
+  tree_all=list.append(tree_all,tree)
+}
+all=data.frame(matrix(ncol = ncol(tree_all[[1]]), nrow = 0))
+colnames(all) <- colnames(tree_all[[1]])
+for (i in 1:length(tree_all)) {all=merge(all,tree_all[[i]],all.x=T,all.y=T)}
 pdf(paste0(output_dir,"/Gene_ontology/treemap_GO_fc1.5_pv0.05.pdf"),12,10)
 treemap(all,index=c("namespace_1003","Description"),vSize="Count",type="categorical",
         inflate.labels = TRUE,vColor = "namespace_1003",fontsize.labels = c(0,0.9),
@@ -73,65 +85,53 @@ treemap(all,index=c("namespace_1003","Description"),vSize="Count",type="categori
 dev.off()
 
 # GO tables for genes
-#BP
-gene=c()
-go=c()
-a=GO_BP@result$geneID
-for (i in 1:length(a)) {
-  b=unlist(strsplit(a[i], split="/"))
-  gene=c(gene,b)
-  go=c(go,rep(GO_BP@result$Description[i],length(b)))
+GO_gene=list()
+for (j in 1:length(res_all)) {
+  gene=c()
+  go=c()
+  a=res_all[[j]]$geneID
+  for (i in 1:length(a)) {
+    b=unlist(strsplit(a[i], split="/"))
+    gene=c(gene,b)
+    go=c(go,rep(res_all[[j]]$Description[i],length(b)))
+  }
+  tab=data.frame(Gene=gene,GO=go)
+  y=table1[,c(1,2)]
+  tab1=merge(tab,y,all.x=T,all.y=F)
+  tab1=tab1[order(tab1$GO),]
+  tab1$GO_domain=res_all[[j]][1,10]
+  GO_gene=list.append(GO_gene,tab1)
 }
-tab_BP=data.frame(Gene=gene,GO=go)
-y=table1[,c(1,2)]
-tab_BP1=merge(tab_BP,y,all.x=T,all.y=F)
-tab_BP1=tab_BP1[order(tab_BP1$GO),]
-tab_BP1$GO_domain="biological_process"
-#CC
-gene=c()
-go=c()
-a=GO_CC@result$geneID
-for (i in 1:length(a)) {
-  b=unlist(strsplit(a[i], split="/"))
-  gene=c(gene,b)
-  go=c(go,rep(GO_CC@result$Description[i],length(b)))
-}
-tab_CC=data.frame(Gene=gene,GO=go)
-tab_CC1=merge(tab_CC,y,all.x=T,all.y=F)
-tab_CC1=tab_CC1[order(tab_CC1$GO),]
-tab_CC1$GO_domain="cellular_component"
-#MF
-gene=c()
-go=c()
-a=GO_MF@result$geneID
-for (i in 1:length(a)) {
-  b=unlist(strsplit(a[i], split="/"))
-  gene=c(gene,b)
-  go=c(go,rep(GO_MF@result$Description[i],length(b)))
-}
-tab_MF=data.frame(Gene=gene,GO=go)
-tab_MF1=merge(tab_MF,y,all.x=T,all.y=F)
-tab_MF1=tab_MF1[order(tab_MF1$GO),]
-tab_MF1$GO_domain="molecular_function"
-# all
-tab=rbind(tab_BP1,tab_CC1,tab_MF1)
-write.csv(tab,paste0(output_dir,"/Gene_ontology/tab_GO_genes.csv"),row.names = F)
+tab_all=do.call("rbind", GO_gene)
+write.csv(tab_all,paste0(output_dir,"/Gene_ontology/tab_GO_genes.csv"),row.names = F)
 
 # dotplot
-pdf(paste0(output_dir,"/Gene_ontology/dotplot_GO.pdf"),10,8)
-dotplot(GO_BP, showCategory=max_c, title = "Enriched GO for Biological Process")
-dotplot(GO_CC, showCategory=max_c, title = "Enriched GO for Cellular Component")
-dotplot(GO_MF, showCategory=max_c, title = "Enriched GO for Molecular Function")
-dev.off()
-png(paste0(output_dir,"/Gene_ontology/dotplot_GO_BP.png"),1000, 800, pointsize=20)
-dotplot(GO_BP, showCategory=max_c, title = "Enriched GO for Biological Process")
-dev.off()
-png(paste0(output_dir,"/Gene_ontology/dotplot_GO_CC.png"),1000, 800, pointsize=20)
-dotplot(GO_CC, showCategory=max_c, title = "Enriched GO for Cellular Component")
-dev.off()
-png(paste0(output_dir,"/Gene_ontology/dotplot_GO_MF.png"),1000, 800, pointsize=20)
-dotplot(GO_MF, showCategory=max_c, title = "Enriched GO for Molecular Function")
-dev.off()
+if (nrow(GO_BP@result)!=0) {
+  pdf(paste0(output_dir,"/Gene_ontology/dotplot_GO_BP.pdf"),10,8)
+  dotplot(GO_BP, showCategory=max_c, title = "Enriched GO for Biological Process")
+  dev.off()
+  png(paste0(output_dir,"/Gene_ontology/dotplot_GO_BP.png"),1000, 800, pointsize=20)
+  dotplot(GO_BP, showCategory=max_c, title = "Enriched GO for Biological Process")
+  dev.off()
+}
+
+if (nrow(GO_CC@result)!=0) {
+  pdf(paste0(output_dir,"/Gene_ontology/dotplot_GO_CC.pdf"),10,8)
+  dotplot(GO_CC, showCategory=max_c, title = "Enriched GO for Cellular Component")
+  dev.off()
+  png(paste0(output_dir,"/Gene_ontology/dotplot_GO_CC.png"),1000, 800, pointsize=20)
+  dotplot(GO_CC, showCategory=max_c, title = "Enriched GO for Cellular Component")
+  dev.off()
+}
+
+if (nrow(GO_MF@result)!=0) {
+  pdf(paste0(output_dir,"/Gene_ontology/dotplot_GO_MF.pdf"),10,8)
+  dotplot(GO_MF, showCategory=max_c, title = "Enriched GO for Molecular Function")
+  dev.off()
+  png(paste0(output_dir,"/Gene_ontology/dotplot_GO_MF.png"),1000, 800, pointsize=20)
+  dotplot(GO_MF, showCategory=max_c, title = "Enriched GO for Molecular Function")
+  dev.off()
+}
 
 #cnetplot
 prov=y[order(y$logFC,decreasing = T),]
@@ -139,32 +139,45 @@ prov=prov[!duplicated(prov$Gene), ]
 prov=prov[!is.na(prov$Gene), ]
 rownames(prov)=prov$Gene
 
-geneList1=prov[unique(tab_BP1$Gene),]
-geneList=geneList1$logFC
-names(geneList)=geneList1$Gene
-pdf(paste0(output_dir,"/Gene_ontology/cnetplot_GO.pdf"),10,8)
+geneList=prov$logFC
+names(geneList)=prov$Gene
+
 par(cex.main=1)
 min=-(round(((max(abs(geneList))+1)-1),1))
 max=round(((max(abs(geneList))+1)+1),1)
-cnetplot.enrichResult(GO_BP,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
+
+if (nrow(GO_BP@result)!=0) {
+  pdf(paste0(output_dir,"/Gene_ontology/cnetplot_GO_BP.pdf"),10,8)
+  cnetplot.enrichResult(GO_BP,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
                       col.bin=seq(min, max, by = 1), main="GO Biological Process FC>1.5 pV<.05")
-cnetplot.enrichResult(GO_CC,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
+  dev.off()
+  png(paste0(output_dir,"/Gene_ontology/cnetplot_GO_BP.png"),1000, 800, pointsize=20)
+  cnetplot.enrichResult(GO_BP,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
+                        col.bin=seq(min, max, by = 1), main="GO Biological Process FC>1.5 pV<.05")
+  dev.off()
+}
+
+if (nrow(GO_CC@result)!=0) {
+  pdf(paste0(output_dir,"/Gene_ontology/cnetplot_GO_CC.pdf"),10,8)
+  cnetplot.enrichResult(GO_CC,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
                       col.bin=seq(min, max, by = 1), main="GO Cellular Component FC>1.5 pV<.05")
-cnetplot.enrichResult(GO_MF,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
+  dev.off()
+  png(paste0(output_dir,"/Gene_ontology/cnetplot_GO_CC.png"),1000, 800, pointsize=20)
+  cnetplot.enrichResult(GO_CC,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
+                        col.bin=seq(min, max, by = 1), main="GO Cellular Component FC>1.5 pV<.05")
+  dev.off()
+}
+
+if (nrow(GO_MF@result)!=0) {
+  pdf(paste0(output_dir,"/Gene_ontology/cnetplot_GO_MF.pdf"),10,8)
+  cnetplot.enrichResult(GO_MF,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
                       col.bin=seq(min, max, by = 1), main="GO Molecular Function FC>1.5 pV<.05")
-dev.off()
-png(paste0(output_dir,"/Gene_ontology/cnetplot_GO_BP.png"),1000, 800, pointsize=20)
-cnetplot.enrichResult(GO_BP,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
-                      col.bin=seq(min, max, by = 1), main="GO Biological Process FC>1.5 pV<.05")
-dev.off()
-png(paste0(output_dir,"/Gene_ontology/cnetplot_GO_CC.png"),1000, 800, pointsize=20)
-cnetplot.enrichResult(GO_CC,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
-                      col.bin=seq(min, max, by = 1), main="GO Cellular Component FC>1.5 pV<.05")
-dev.off()
-png(paste0(output_dir,"/Gene_ontology/cnetplot_GO_MF.png"),1000, 800, pointsize=20)
-cnetplot.enrichResult(GO_MF,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
+  dev.off()
+  png(paste0(output_dir,"/Gene_ontology/cnetplot_GO_MF.png"),1000, 800, pointsize=20)
+  cnetplot.enrichResult(GO_MF,categorySize="pvalue", foldChange=geneList, showCategory = max_c,
                       col.bin=seq(min, max, by = 1), main="GO Molecular Function FC>1.5 pV<.05")
-dev.off()
+  dev.off()
+}
 
 ##############################################
 ######## Pathway enrichment analysis #########
