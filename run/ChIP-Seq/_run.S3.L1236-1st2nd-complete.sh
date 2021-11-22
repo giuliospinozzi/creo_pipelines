1e36989258579d456930a95121fc311caa6f0446
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2021-11-22 19:34:25 +0100

    [NEW] ChIP-Seq run added

---
+++ run/ChIP-Seq/_run.S3.L1236-1st2nd-complete.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+
+#L1236
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep1-NT-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_NT_INPUT/Clean_L1236_R1_NT_INPUT_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_NT_INPUT/Clean_L1236_R1_NT_INPUT_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep1-NT-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_NT_STAT6/Clean_L1236_R1_NT_STAT6_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_NT_STAT6/Clean_L1236_R1_NT_STAT6_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep1-NT-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_NT_pSTAT6/Clean_L1236_R1_NT_pSTAT6_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_NT_pSTAT6/Clean_L1236_R1_NT_pSTAT6_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep1-KO-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_KO_INPUT/Clean_L1236_R1_KO_INPUT_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_KO_INPUT/Clean_L1236_R1_KO_INPUT_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep1-KO-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_KO_STAT6/Clean_L1236_R1_KO_STAT6_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_KO_STAT6/Clean_L1236_R1_KO_STAT6_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep1-KO-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_KO_pSTAT6/Clean_L1236_R1_KO_pSTAT6_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R1_KO_pSTAT6/Clean_L1236_R1_KO_pSTAT6_2.fq.gz
+
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-NT-INPUTa /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_INPUTa/Clean_L1236_R2_NT_INPUTa_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_INPUTa/Clean_L1236_R2_NT_INPUTa_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-NT-INPUTb /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_INPUTb/Clean_L1236_R2_NT_INPUTb_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_INPUTb/Clean_L1236_R2_NT_INPUTb_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-NT-STAT6a /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_STAT6a/Clean_L1236_R2_NT_STAT6a_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_STAT6a/Clean_L1236_R2_NT_STAT6a_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-NT-pSTAT6b /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_pSTAT6b/Clean_L1236_R2_NT_pSTAT6b_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_NT_pSTAT6b/Clean_L1236_R2_NT_pSTAT6b_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-KO-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_KO_INPUT/Clean_L1236_R2_KO_INPUT_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_KO_INPUT/Clean_L1236_R2_KO_INPUT_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-KO-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_KO_STAT6/Clean_L1236_R2_KO_STAT6_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_KO_STAT6/Clean_L1236_R2_KO_STAT6_2.fq.gz
+ChIP-Seq_pipeline.PE Chip-Tasselli L1236 L1236-Rep2-KO-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19 /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_KO_pSTAT6/Clean_L1236_R2_KO_pSTAT6_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F21FTSEUHT0938_HUMszacR/upload/Fq/L1236_R2_KO_pSTAT6/Clean_L1236_R2_KO_pSTAT6_2.fq.gz
+
+cd /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks
+closest-features --closest Clean_L1236_R1_KO_INPUT_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R1_KO_INPUT_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_KO_INPUT_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_KO_INPUT_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R1_KO_STAT6_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R1_KO_STAT6_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_KO_STAT6_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_KO_STAT6_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R1_KO_pSTAT6_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R1_KO_pSTAT6_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_KO_pSTAT6_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_KO_pSTAT6_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R1_NT_INPUT_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R1_NT_INPUT_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_NT_INPUTa_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_NT_INPUTa_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_NT_INPUTb_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_NT_INPUTb_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R1_NT_STAT6_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R1_NT_STAT6_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_NT_STAT6a_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_NT_STAT6a_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R1_NT_pSTAT6_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R1_NT_pSTAT6_1_summits.annotated.bed
+closest-features --closest Clean_L1236_R2_NT_pSTAT6b_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Clean_L1236_R2_NT_pSTAT6b_1_summits.annotated.bed
+
+
+
+# Diff Peaks
+mkdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-NT-STAT6/Clean_L1236_R1_NT_STAT6_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-NT-INPUT/Clean_L1236_R1_NT_INPUT_1.sorted.filtered.bam -f BAMPE -n NT-STAT6-INPUT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-NT-pSTAT6/Clean_L1236_R1_NT_pSTAT6_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-NT-INPUT/Clean_L1236_R1_NT_INPUT_1.sorted.filtered.bam -f BAMPE -n NT-pSTAT6-INPUT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-NT-STAT6a/Clean_L1236_R2_NT_STAT6a_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-NT-INPUTa/Clean_L1236_R2_NT_INPUTa_1.sorted.filtered.bam -f BAMPE -n NT-STAT6a-INPUTa --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-NT-pSTAT6b/Clean_L1236_R2_NT_pSTAT6b_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-NT-INPUTb/Clean_L1236_R2_NT_INPUTb_1.sorted.filtered.bam -f BAMPE -n NT-pSTAT6b-INPUTb --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-NT-STAT6/Clean_L1236_R1_NT_STAT6_1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-NT-STAT6/Clean_L1236_R2_NT_STAT6_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-NT-INPUT/Clean_L1236_R1_NT_INPUT_1.sorted.filtered.bam -f BAMPE -n NT-STAT6-INPUT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-KO-STAT6/Clean_L1236_R1_KO_STAT6_1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-KO-STAT6/Clean_L1236_R2_KO_STAT6_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-KO-INPUT/Clean_L1236_R1_KO_INPUT_1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-KO-INPUT/Clean_L1236_R2_KO_INPUT_1.sorted.filtered.bam -f BAMPE -n KO-STAT6-INPUT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-KO-pSTAT6/Clean_L1236_R1_KO_pSTAT6_1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-KO-pSTAT6/Clean_L1236_R2_KO_pSTAT6_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep1-KO-INPUT/Clean_L1236_R1_KO_INPUT_1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/L1236/bam/L1236-Rep2-KO-INPUT/Clean_L1236_R2_KO_INPUT_1.sorted.filtered.bam -f BAMPE -n KO-pSTAT6-INPUT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+
+
+cd /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/combinations
+closest-features --closest KO-STAT6-INPUT_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > KO-STAT6-INPUT_summits.annotated.bed
+closest-features --closest KO-pSTAT6-INPUT_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > KO-pSTAT6-INPUT_summits.annotated.bed
+closest-features --closest NT-STAT6-INPUT_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > NT-STAT6-INPUT_summits.annotated.bed
+closest-features --closest NT-STAT6a-INPUTa_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > NT-STAT6a-INPUTa_summits.annotated.bed
+closest-features --closest NT-pSTAT6-INPUT_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > NT-pSTAT6-INPUT_summits.annotated.bed
+closest-features --closest NT-pSTAT6b-INPUTb_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > NT-pSTAT6b-INPUTb_summits.annotated.bed
+
+
+mkdir /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/irreproducible_discovery_rate/
+cd /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/irreproducible_discovery_rate/
+for k in $( ls /opt/ngs/results_tiacci/Chip-Tasselli/L1236/peaks/*.narrowPeak ); do echo $k; cat $k | sort -k8,8nr | head -n 100000 > ${k:0:-10}sorted.narrowPeak; done
+mv ../*.sorted.narrowPeak .
+idr --samples Clean_L1236_R1_KO_pSTAT6_1_peaks.sorted.narrowPeak Clean_L1236_R2_KO_pSTAT6_1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file KO_pSTAT6-idr --plot --log-output-file KO_pSTAT6.idr.log
+idr --samples Clean_L1236_R1_KO_STAT6_1_peaks.sorted.narrowPeak Clean_L1236_R2_KO_STAT6_1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file KO_STAT6-idr --plot --log-output-file KO_STAT6.idr.log
+idr --samples Clean_L1236_R1_KO_INPUT_1_peaks.sorted.narrowPeak Clean_L1236_R2_KO_INPUT_1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file KO_INPUT-idr --plot --log-output-file KO_INPUT.idr.log
+
