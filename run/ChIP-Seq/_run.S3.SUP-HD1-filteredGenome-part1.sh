52e55109ada2dbe61761cecb96d6b40e8a041e8e
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2022-05-27 11:58:40 +0200

    [NEW] ChIP-Seq Tiacci SUP-HD1

---
+++ run/ChIP-Seq/_run.S3.SUP-HD1-filteredGenome-part1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+#SUP-HD1
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 4-A-S-KO-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/4-A-S-KO-INPUT/4-A-S-KO-INPUT_L1_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/4-A-S-KO-INPUT/4-A-S-KO-INPUT_L1_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 4-B-S-KO-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/4-B-S-KO-INPUT/4-B-S-KO-INPUT_L2_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/4-B-S-KO-INPUT/4-B-S-KO-INPUT_L2_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 6-A-S-KO-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/6-A-S-KO-pSTAT6/6-A-S-KO-pSTAT6_L1_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/6-A-S-KO-pSTAT6/6-A-S-KO-pSTAT6_L1_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 6-B-S-KO-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/6-B-S-KO-pSTAT/6-B-S-KO-pSTAT_L2_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/6-B-S-KO-pSTAT/6-B-S-KO-pSTAT_L2_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 5-A-S-KO-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/5-A-5-KO-STAT6/5-A-5-KO-STAT6_L1_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/5-A-5-KO-STAT6/5-A-5-KO-STAT6_L1_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 5-B-S-KO-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/5-B-S-KO-STAT6/5-B-S-KO-STAT6_L2_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/5-B-S-KO-STAT6/5-B-S-KO-STAT6_L2_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 1-A-S-NT-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/1-A-S-NT-INPUT/1-A-S-NT-INPUT_L1_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/1-A-S-NT-INPUT/1-A-S-NT-INPUT_L1_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 1-B-S-NT-INPUT /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/1-B-S-NT-INPUT/1-B-S-NT-INPUT_L2_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/1-B-S-NT-INPUT/1-B-S-NT-INPUT_L2_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 3-A-S-NT-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/3-A-S-NT-pSTAT6/3-A-S-NT-pSTAT6_L1_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/3-A-S-NT-pSTAT6/3-A-S-NT-pSTAT6_L1_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 3-B-S-NT-pSTAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/3-B-S-NT-pSTAT6/3-B-S-NT-pSTAT6_L2_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/3-B-S-NT-pSTAT6/3-B-S-NT-pSTAT6_L2_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 2-A-S-NT-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/2-A-S-NT-STAT6/2-A-S-NT-STAT6_L1_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/2-A-S-NT-STAT6/2-A-S-NT-STAT6_L1_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli SUP-HD1 2-B-S-NT-STAT6 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/2-B-S-NT-STAT6/2-B-S-NT-STAT6_L2_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0469_HUMrqybR/2-B-S-NT-STAT6/2-B-S-NT-STAT6_L2_2.fq.gz 
+
+
+cd /opt/ngs/results_tiacci/Chip-Tasselli/SUP-HD1/peaks
+closest-features --closest 4-A-S-KO-INPUT_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 4-A-S-KO-INPUT_L1_1_summits.annotated.bed
+closest-features --closest 4-B-S-KO-INPUT_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 4-B-S-KO-INPUT_L1_1_summits.annotated.bed
+closest-features --closest 6-A-S-KO-pSTAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 6-A-S-KO-pSTAT6_L1_1_summits.annotated.bed
+closest-features --closest 6-B-S-KO-pSTAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 6-B-S-KO-pSTAT6_L1_1_summits.annotated.bed
+closest-features --closest 5-A-S-KO-STAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 5-A-S-KO-STAT6_L1_1_summits.annotated.bed
+closest-features --closest 5-B-S-KO-STAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 5-B-S-KO-STAT6_L1_1_summits.annotated.bed
+closest-features --closest 1-A-S-NT-INPUT_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 1-A-S-NT-INPUT_L1_1_summits.annotated.bed
+closest-features --closest 1-B-S-NT-INPUT_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 1-B-S-NT-INPUT_L1_1_summits.annotated.bed
+closest-features --closest 3-A-S-NT-pSTAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 3-A-S-NT-pSTAT6_L1_1_summits.annotated.bed
+closest-features --closest 3-B-S-NT-pSTAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 3-B-S-NT-pSTAT6_L1_1_summits.annotated.bed
+closest-features --closest 2-A-S-NT-STAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 2-A-S-NT-STAT6_L1_1_summits.annotated.bed
+closest-features --closest 2-B-S-NT-STAT6_L1_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > 2-B-S-NT-STAT6_L1_1_summits.annotated.bed
