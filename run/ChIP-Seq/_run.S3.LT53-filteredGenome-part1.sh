855d5129f3b90af49613761396f2356c7d7bcecf
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2022-08-11 12:03:29 +0200

    [NEW] LT53 ChIP-Seq (Tasselli)

---
+++ run/ChIP-Seq/_run.S3.LT53-filteredGenome-part1.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+#LT53
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-Empty-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_Empty/INPUT_Empty_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_Empty/INPUT_Empty_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-WT-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_WT/INPUT_WT_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_WT/INPUT_WT_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-N417Y-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_N417Y/INPUT_N417Y_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_N417Y/INPUT_N417Y_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-D419G-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_D419N/INPUT_D419N_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_D419N/INPUT_D419N_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-N417Y-D419G-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_N417Y_D419G/INPUT_N417Y_D419G_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_N417Y_D419G/INPUT_N417Y_D419G_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-D419N-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_D419N/INPUT_D419N_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_D419N/INPUT_D419N_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 INPUT-N417Y-D419N-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_N417Y_D419N/INPUT_N417Y_D419N_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/INPUT_N417Y_D419N/INPUT_N417Y_D419N_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 Empty-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/Empty/Empty_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/Empty/Empty_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 WT-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/WT/WT_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/WT/WT_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 N417Y-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/N417Y/N417Y_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/N417Y/N417Y_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 D419G-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/D419N/D419N_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/D419N/D419N_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 N417Y-D419G-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/N417Y_D419G/N417Y_D419G_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/N417Y_D419G/N417Y_D419G_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 D419N-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/D419N/D419N_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/D419N/D419N_2.fq.gz 
+
+ChIP-Seq_pipeline.PE Chip-Tasselli LT53 N417Y-D419N-1 /opt/ngs/results_tiacci 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/N417Y_D419N/N417Y_D419N_1.fq.gz /mnt/Martelli_Collaborations/Tiacci/Chip-Tasselli-Amazon/F22FTSEUHT0912_HUMlzdjR/N417Y_D419N/N417Y_D419N_2.fq.gz 
+
+
+cd /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks
+closest-features --closest INPUT_Empty_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_Empty_1_summits.annotated.bed
+closest-features --closest INPUT_WT_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_WT_1_summits.annotated.bed
+closest-features --closest INPUT_N417Y_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_N417Y_1_summits.annotated.bed
+closest-features --closest INPUT_D419N_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_D419N_1_summits.annotated.bed
+closest-features --closest INPUT_N417Y_D419G_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_N417Y_D419G_1_summits.annotated.bed
+closest-features --closest INPUT_D419N_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_D419N_1_summits.annotated.bed
+closest-features --closest INPUT_N417Y_D419N_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT_N417Y_D419N_1_summits.annotated.bed
+closest-features --closest Empty_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > Empty_1_summits.annotated.bed
+closest-features --closest WT_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > WT_1_summits.annotated.bed
+closest-features --closest N417Y_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > N417Y_1_summits.annotated.bed
+closest-features --closest D419N_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > D419N_1_summits.annotated.bed
+closest-features --closest N417Y_D419G_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > N417Y_D419G_1_summits.annotated.bed
+closest-features --closest D419N_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > D419N_1_summits.annotated.bed
+closest-features --closest N417Y_D419N_1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > N417Y_D419N_1_summits.annotated.bed
---
+++ run/ChIP-Seq/_run.S3.LT53-filteredGenome-part2-perReplicate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+
+#LT53 splitted
+mkdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations
+mkdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT_Empty-1/Empty_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT_Empty-1/INPUT_Empty_1.sorted.filtered.bam -f BAMPE -n INPUT-Empty-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-WT-1/WT_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-WT-1/INPUT_WT_1.sorted.filtered.bam -f BAMPE -n INPUT-WT-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-N417Y-1/N417Y_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-N417Y-1/INPUT_N417Y_1.sorted.filtered.bam -f BAMPE -n INPUT-N417Y-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-D419G-1/D419G_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-D419G-1/INPUT_D419G_1.sorted.filtered.bam -f BAMPE -n INPUT-D419G-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-N417Y-D419G-1/N417Y_D419G_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-N417Y-D419G-1/INPUT_N417Y_D419G_1.sorted.filtered.bam -f BAMPE -n INPUT-N417Y-D419G-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-D419N-1/D419N_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-D419N-1/INPUT_D419N_1.sorted.filtered.bam -f BAMPE -n INPUT-D419N-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+macs2 callpeak --pvalue 0.0001 -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-N417Y-D419N-1/N417Y_D419N_1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT53/bam/INPUT-N417Y-D419N-1/INPUT_N417Y_D419N_1.sorted.filtered.bam -f BAMPE -n INPUT-N417Y-D419N-1 --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+
+cd /opt/ngs/results_tiacci/Chip-Tasselli/LT53/peaks/combinations/filtered
+
+closest-features --closest INPUT-Empty-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-Empty-1_summits.annotated.bed
+closest-features --closest INPUT-WT-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-WT-1_summits.annotated.bed
+closest-features --closest INPUT-N417Y-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-N417Y-1_summits.annotated.bed
+closest-features --closest INPUT-D419G-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-D419G-1_summits.annotated.bed
+closest-features --closest INPUT-N417Y-D419G-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-N417Y-D419G-1_summits.annotated.bed
+closest-features --closest INPUT-D419N-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-D419N-1_summits.annotated.bed
+closest-features --closest INPUT-N417Y-D419N-1_summits.bed <(gtf2bed < /opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf) > INPUT-N417Y-D419N-1_summits.annotated.bed
+
