2f2d2e8d0d9c8149114ad1b295b2ed2ae47fb195
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2022-09-15 13:25:00 +0200

    [NEW] ChIP-Seq Scialdone 1stBatch part1

---
+++ run/ChIP-Seq/_run.Scialdone.1stBatch.mm10-part1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+#Pilot-1
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch A1-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A1-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A1-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch A2-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A2-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A2-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch A3-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A3-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A3-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch A4-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A4-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/A4-27422_R2_001.fastq.gz
+
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch B2-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/B2-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/B2-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch B3-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/B3-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/B3-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch B4-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/B4-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/B4-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch BCinput-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/BCinput_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/BCinput_R2_001.fastq.gz
+
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch C1-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C1-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C1-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch C2-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C2-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C2-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch C3-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C3-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C3-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch C4-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C4-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/C4-27422_R2_001.fastq.gz
+
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch D2-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/D2-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/D2-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch D3-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/D3-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/D3-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch D4-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/D4-27422_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/D4-27422_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch DN-BCinput-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/DN-BCinput_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/DN-BCinput_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch DNinput-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/DNinput_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/DNinput_R2_001.fastq.gz
+ChIP-Seq_pipeline.PE Scialdone ChIP-1stBatch WTinput-1 /opt/ngs/results_falini 12 /opt/genome/mouse/mm10/index/bowtie2/mm10.filtered /opt/genome/control/phix174/bwa/phiX174.fa /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/WTinput_R1_001.fastq.gz /mnt/Martelli_Collaborations/Scialdone/90-680230713/00_fastq/WTinput_R2_001.fastq.gz
+
+
+cd /opt/ngs/results_falini/Scialdone/ChIP-1stBatch/peaks
+closest-features --closest A1-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > A1-27422_R1_001_summits.annotated.bed
+closest-features --closest A2-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > A2-27422_R1_001_summits.annotated.bed
+closest-features --closest A3-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > A3-27422_R1_001_summits.annotated.bed
+closest-features --closest A4-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > A4-27422_R1_001_summits.annotated.bed
+
+closest-features --closest B2-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > B2-27422_R1_001_summits.annotated.bed
+closest-features --closest B3-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > B3-27422_R1_001_summits.annotated.bed
+closest-features --closest B4-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > B4-27422_R1_001_summits.annotated.bed
+closest-features --closest BCinput_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > BCinput_R1_001_summits.annotated.bed
+
+closest-features --closest C1-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > C1-27422_R1_001_summits.annotated.bed
+closest-features --closest C2-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > C2-27422_R1_001_summits.annotated.bed
+closest-features --closest C3-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > C3-27422_R1_001_summits.annotated.bed
+closest-features --closest C4-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > C4-27422_R1_001_summits.annotated.bed
+
+closest-features --closest D2-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > D2-27422_R1_001_summits.annotated.bed
+closest-features --closest D3-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > D3-27422_R1_001_summits.annotated.bed
+closest-features --closest D4-27422_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > D4-27422_R1_001_summits.annotated.bed
+closest-features --closest DN-BCinput_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > DN-BCinput_R1_001_summits.annotated.bed
+closest-features --closest DNinput_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > DNinput_R1_001_summits.annotated.bed
+closest-features --closest WTinput_R1_001_summits.bed <(gtf2bed < /opt/genome/mouse/mm10/annotation/mm10.sorted.gtf) > WTinput_R1_001_summits.annotated.bed
