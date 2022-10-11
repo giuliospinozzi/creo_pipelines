9b661578b43364a0dd0216809f7076694f23cf6e
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2022-10-11 09:13:51 +0200

    [NEW] first beta version of ATAC-Seq Pipeline and first run on 448 pool

---
+++ pipelines/ATAC-Seq.pipeline-PE.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+
+echo "
+  +--------------------------------------------------------+
+  |                                                        |
+  |                ATAC-Seq Pipeline (CREO)                |
+  |                                                        |
+  +--------------------------------------------------------+
+  |  Author:   Giulio Spinozzi                             |
+  |  Date:     September 2022                              |
+  |  Contact:  giulio.spinozzi@unipg.it                    |
+  |  Version:  1.0 - PE                                    |
+  +--------------------------------------------------------+
+
+  REQUIRED VARS and relative ORDER POSITION -> REMEMBER NO SPACES!!!!!!!!!
+	1. PROJECT_NAME [Brunetti]
+	2. POOL_NAME [ChIP-Seq]
+	3. LIBRARY_NAME [library290817A1]
+	4. RESULTS_DIR [/opt/ngs/results]
+	5. MAXTHREADS [12]
+	6. REFERENCE_GENOME [/opt/genome/human/hg19/index/bwa/hg19.fa, /opt/genome/human/hg19/index/bowtie2/hg19]
+	7. PHIX_GENOME [/opt/genome/control/phix174/bwa/phiX174.fa, /opt/genome/control/phix174/bowtie2/phiX174.fa]
+	8. READ1 [R1.fastq.gz]
+	9. READ1 [R2.fastq.gz]
+	
+  NOTES:
+  	- Input are Paired End Reads. 
+"
+
+
+echo "<`date +'%Y-%m-%d %H:%M:%S'`> [CREO-UniPG] Preprocessing input variables (delimiters:<>)"
+## print input variables (check for log utils)
+INPUTVARNUM=0
+for INPUTVAR in "$@"; do
+	let INPUTVARNUM++; 
+	printf -v INPUTNUM '%02d' $INPUTVARNUM;
+    echo "  => Input Variable: Order number = <${INPUTNUM}> ; Var Content = <${INPUTVAR}>";
+done
+
+
+
+##### =================== Start SETTINGS ======================= #####
+PROJECT_NAME="${1}";
+POOL_NAME="${2}";
+LIBRARY_NAME="${3}";
+RESULTS_DIR="${4}";
+MAXTHREADS="${5}";
+REFERENCE_GENOME="${6}";
+PHIX_GENOME="${7}";
+R1_FASTQ="${8}";
+R2_FASTQ="${9}";
+
+printf "Folder creation --> ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}\n"
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/quality
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/quality/${LIBRARY_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/stats
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/peaks
+
+
+RUN_NAME="${PROJECT_NAME}|${POOL_NAME}|${LIBRARY_NAME}"
+NUMBER_RAW_READS=$((`zcat ${R1_FASTQ} | wc -l`/2)) ;
+##### ==================== End SETTINGS ======================== #####
+
+cp -rf ${R1_FASTQ} ${R2_FASTQ} ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}
+# identify base names
+BNAME_R1="`basename ${R1_FASTQ}`";
+BNAME_R2="`basename ${R2_FASTQ}`";
+R1_FASTQ=${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R1};
+R2_FASTQ=${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R2};
+
+
+##### ================== RAW DATA QUALITY ====================== #####
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### FastQC Report #######\n"
+fastqc --nogroup --extract -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/quality/${LIBRARY_NAME} -t ${MAXTHREADS} -f fastq ${R1_FASTQ} ${R2_FASTQ}
+
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### FastQ Screen Report #######\n"
+fastq_screen --threads ${MAXTHREADS} ${R1_FASTQ} ${R2_FASTQ} --outdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/quality/${LIBRARY_NAME}
+BNAME_R1=`basename ${R1_FASTQ} | sed 's/.gz//g' | cut -d'.' -f1`;
+BNAME_R2=`basename ${R2_FASTQ} | sed 's/.gz//g' | cut -d'.' -f1`;
+##### ========================================================== #####
+
+
+#### ==================== PhiX Alignment ====================== #####
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### PhiX Alignment #######\n"
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}
+mkdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}
+bwa mem -k 16 -r 1 -M -T 15 -t ${MAXTHREADS} -v 1 ${PHIX_GENOME} <(zcat ${R1_FASTQ} ) <(zcat ${R2_FASTQ} ) | samtools view -F 2308 -q 25 -f 35 -uS - > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.bam;
+samtools view ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.bam | cut -f 1 > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.list
+sort --parallel=5 ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.list > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.sorted.list;
+rm ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.list;
+zcat ${R1_FASTQ} | fqreverseextract.pureheader ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.sorted.list | pigz --best -f -c > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R1}_nophix.fastq.gz &
+zcat ${R2_FASTQ} | fqreverseextract.pureheader ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.sorted.list | pigz --best -f -c > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R2}_nophix.fastq.gz &
+wait
+NUMBER_PHIX_READS=$((`wc -l ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.sorted.list | cut -d' ' -f1 `*2)) ;
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ##### PhiX READS: ${NUMBER_PHIX_READS} #####\n"
+pigz -f ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/phix/${LIBRARY_NAME}/${BNAME_R1}.PhiX.header.sorted.list;
+rm ${R1_FASTQ} ${R2_FASTQ}
+R1_FASTQ="${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R1}_nophix.fastq.gz";
+R2_FASTQ="${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R2}_nophix.fastq.gz";
+#### ========================================================== #####
+
+
+##### ================= ATAC Adapter Removal =================== #####
+ADAPTER="CTGTCTCTTATACACATCT"
+cutadapt -a "${ADAPTER}" -A "${ADAPTER}" -j ${MAXTHREADS} -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R1}_noAdapter.fastq.gz -p ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R2}_noAdapter.fastq.gz ${R1_FASTQ} ${R2_FASTQ}
+rm ${R1_FASTQ} ${R2_FASTQ}
+R1_FASTQ="${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R1}_noAdapter.fastq.gz";
+R2_FASTQ="${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq/${LIBRARY_NAME}/${BNAME_R2}_noAdapter.fastq.gz";
+##### ========================================================== #####
+
+
+
+##### ================= Ref Genome Alignment =================== #####
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### Reference Genome Alignment #######\n"
+bowtie2 -p ${MAXTHREADS} -x ${REFERENCE_GENOME} -1 <(zcat ${R1_FASTQ} ) -2 <(zcat ${R2_FASTQ} ) --no-unal | samtools view -bS - > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.bam;
+samtools sort -@ ${MAXTHREADS} ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.bam -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.bam
+samtools index ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.bam
+rm ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.bam;
+READS_MAPPED=$((`samtools view -F 0x04 -c ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.bam`)) ;
+picard-tools MarkDuplicates I=${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.bam O=${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam M=${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}_duplicates.txt REMOVE_DUPLICATES=true
+samtools index ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam;
+READS_MAPPED_NODUPLICATES=$((`samtools view -F 0x04 -c ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam`)) ;
+# bedtools bamtobed -cigar -i ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.bed
+# printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### After Alignment: Coverage Analysis #######\n"
+# bamCoverage -p ${MAXTHREADS} -b ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam --outFileFormat bigwig -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.bw &
+# bamCoverage -p ${MAXTHREADS} -b ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam --outFileFormat bedgraph -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.bedgraph &
+# wait
+rm ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.bam*
+##### ========================================================== #####
+
+
+
+##### ================== Alignment Filtering =================== #####
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### Filtering #######\n"
+bamtools filter -in ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam -isMapped true -isPrimaryAlignment true -mapQuality ">=20" -out ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam
+samtools index ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam
+#bedtools bamtobed -cigar -i ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam | grep '/1' > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bed
+bedtools bamtobed -cigar -i ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bed
+READS_AFTER_FILTERS=`wc -l ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bed | cut -d' ' -f1 ` ;
+printf "<`date +'%Y-%m-%d %H:%M:%S'`> ####### After Filtering: Coverage Analysis #######\n"
+# --effectiveGenomeSize is for hg19!!!!!
+bamCoverage -p ${MAXTHREADS} -b ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam --outFileFormat bigwig -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bw &
+bamCoverage -p ${MAXTHREADS} -b ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam --outFileFormat bedgraph -o ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bedgraph &
+wait
+rm ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.noDuplicates.bam*
+bamPEFragmentSize -b ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam -hist ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.histLengths.png -p ${MAXTHREADS} -T ${BNAME_R1} -bs 1 --samplesLabel ${BNAME_R1}
+##### ========================================================== #####
+
+##### ==================== Peak Detection ====================== #####
+macs2 callpeak -t ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bam/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bam -f BAMPE -n ${BNAME_R1} --outdir ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/peaks
+##### ========================================================== #####
+
+##### ========================= Stats ========================== #####
+if [ ! -r "${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/stats/sample_report.csv" ]; then
+		echo -e "RUN_NAME\tNUMBER_RAW_READS\tNUMBER_PHIX_READS\tMAPPED_READS\tREADS_MAPPED_NODUPLICATES\tREADS_AFTER_FILTERS" > ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/stats/sample_report.csv
+	fi
+echo -e "${RUN_NAME}\t${NUMBER_RAW_READS}\t${NUMBER_PHIX_READS}\t${READS_MAPPED}\t${READS_MAPPED_NODUPLICATES}\t${READS_AFTER_FILTERS}" >> ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/stats/sample_report.csv
+##### ========================================================== #####
+
+##### ========================= Reducing File size ========================== #####
+pigz -f ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/bed/${LIBRARY_NAME}/${BNAME_R1}.sorted.filtered.bed
+rm -rf ${RESULTS_DIR}/${PROJECT_NAME}/${POOL_NAME}/fastq
+##### ======================================================================= #####
+
+
+
---
+++ run/ATAC-Seq/_run.Martelli.488.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source /etc/environment
+source /etc/profile
+
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_A_EMPTY /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_A_EMPTY_S1_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_A_EMPTY_S1_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_A_MUT /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_A_MUT_S2_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_A_MUT_S2_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_A_WT /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_A_WT_S3_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_A_WT_S3_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_B_EMPTY /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_B_EMPTY_S4_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_B_EMPTY_S4_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_B_MUT /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_B_MUT_S5_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_B_MUT_S5_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_B_WT /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_B_WT_S6_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_B_WT_S6_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_C_EMPTY /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_C_EMPTY_S7_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_C_EMPTY_S7_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_C_MUT /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_C_MUT_S8_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_C_MUT_S8_R2_001.fastq.gz
+
+ATAC-Seq.pipeline-PE ATAC-Seq run488 IDH2_C_WT /opt/ngs/results 12 /opt/genome/human/hg19/index/bowtie2/hg19.filtered /opt/genome/control/phix174/bwa/phiX174.fa /home/giulio/ATAC-OSR-run488/IDH2_C_WT_S9_R1_001.fastq.gz /home/giulio/ATAC-OSR-run488/IDH2_C_WT_S9_R2_001.fastq.gz
