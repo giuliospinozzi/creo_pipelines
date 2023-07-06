16ba9cbd1efd1d5d490b4f88812d3be2559942cc
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2023-07-06 09:42:44 +0200

    [FIX] LT67 ChIP-Seq fixes on part2

--- run/ChIP-Seq/_run.S3.LT67-part2.sh
+++ run/ChIP-Seq/_run.S3.LT67-part2.sh
@@ -8,7 +8,7 @@ mkdir /opt/ngs/results_tiacci/Chip-Tasselli/LT67/peaks/combinations/merged
 #L67
 macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-Empty-ReplA/1_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-Empty-ReplB/15_R1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/Empty-ReplA/8_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/Empty-ReplB/22_R1.sorted.filtered.bam -f BAMPE -n INPUT-Empty --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT67/peaks/combinations/merged
 
-macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-STAT6-WT-ReplA/2_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-STAT6-WT-Replb/16_R1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/STAT6-WT-ReplA/9_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/STAT6-WT-ReplB/23_R1.sorted.filtered.bam -f BAMPE -n INPUT-STAT6-WT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT67/peaks/combinations/merged
+macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-STAT6-WT-ReplA/2_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-STAT6-WT-ReplB/16_R1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/STAT6-WT-ReplA/9_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/STAT6-WT-ReplB/23_R1.sorted.filtered.bam -f BAMPE -n INPUT-STAT6-WT --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT67/peaks/combinations/merged
 
 macs2 callpeak -B -g hs -t /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-STAT6-N417Y-ReplA/3_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/INPUT-STAT6-N417Y-ReplB/17_R1.sorted.filtered.bam -c /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/STAT6-N417Y-ReplA/10_R1.sorted.filtered.bam /opt/ngs/results_tiacci/Chip-Tasselli/LT67/bam/STAT6-N417Y-ReplB/24_R1.sorted.filtered.bam -f BAMPE -n INPUT-STAT6-N417Y --outdir /opt/ngs/results_tiacci/Chip-Tasselli/LT67/peaks/combinations/merged
 
@@ -48,12 +48,12 @@ idr --samples 6_R1_peaks.sorted.narrowPeak 20_R1_peaks.sorted.narrowPeak --input
 idr --samples 7_R1_peaks.sorted.narrowPeak 21_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file INPUT-STAT6-N417Y-D419N-idr --plot --log-output-file INPUT-STAT6-N417Y-D419N.idr.log
 
 idr --samples 8_R1_peaks.sorted.narrowPeak 22_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file Empty-idr --plot --log-output-file Empty.idr.log
-idr --samples 9_R1_peaks.sorted.narrowPeak 23_R1_peaks.sorted.narrowPeak --file-type narrowPeak --rank p.value --output-file STAT6-WT-idr --plot --log-output-file STAT6-WT.idr.log
-idr --samples 10_R1_peaks.sorted.narrowPeak 24_R1_peaks.sorted.narrowPeak --file-type narrowPeak --rank p.value --output-file STAT6-N417Y-idr --plot --log-output-file STAT6-N417Y.idr.log
-idr --samples 11_R1_peaks.sorted.narrowPeak 25_R1_peaks.sorted.narrowPeak --file-type narrowPeak --rank p.value --output-file STAT6-D419G-idr --plot --log-output-file STAT6-D419G.idr.log
-idr --samples 12_R1_peaks.sorted.narrowPeak 26_R1_peaks.sorted.narrowPeak --file-type narrowPeak --rank p.value --output-file STAT6-N417Y-D419G-idr --plot --log-output-file STAT6-N417Y-D419G.idr.log
-idr --samples 13_R1_peaks.sorted.narrowPeak 27_R1_peaks.sorted.narrowPeak --file-type narrowPeak --rank p.value --output-file STAT6-D419N-idr --plot --log-output-file STAT6-D419N.idr.log
-idr --samples 14_R1_peaks.sorted.narrowPeak 28_R1_peaks.sorted.narrowPeak --file-type narrowPeak --rank p.value --output-file STAT6-N417Y-D419N-idr --plot --log-output-file STAT6-N417Y-D419N.idr.log
+idr --samples 9_R1_peaks.sorted.narrowPeak 23_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file STAT6-WT-idr --plot --log-output-file STAT6-WT.idr.log
+idr --samples 10_R1_peaks.sorted.narrowPeak 24_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file STAT6-N417Y-idr --plot --log-output-file STAT6-N417Y.idr.log
+idr --samples 11_R1_peaks.sorted.narrowPeak 25_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file STAT6-D419G-idr --plot --log-output-file STAT6-D419G.idr.log
+idr --samples 12_R1_peaks.sorted.narrowPeak 26_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file STAT6-N417Y-D419G-idr --plot --log-output-file STAT6-N417Y-D419G.idr.log
+idr --samples 13_R1_peaks.sorted.narrowPeak 27_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file STAT6-D419N-idr --plot --log-output-file STAT6-D419N.idr.log
+idr --samples 14_R1_peaks.sorted.narrowPeak 28_R1_peaks.sorted.narrowPeak --input-file-type narrowPeak --rank p.value --output-file STAT6-N417Y-D419N-idr --plot --log-output-file STAT6-N417Y-D419N.idr.log
 
 
 
