c95f66ade4c6d19b818c3b256f0c05559bf459cd
Giulio Spinozzi, PhD <giuliospinozzi.phd@gmail.com>
2025-04-09 15:40:25 +0200

    [NEW] Invasion Project 2504 Comparison1

---
+++ run/RNA-Seq/250409_opbg_genomix4life_InvasionProject_confronto1.sh
@@ -0,0 +1,80 @@
+# Replace the variables with paths and options of interest.
+TODAY=`date +"%Y%m%d%H%M%S"`
+# Project name.
+PROJECT_NAME="results_opbg";
+
+# Pool name.
+POOL_NAME="InvasionProject_250409_comparison1";
+
+# Sample names (',' sep).
+SAMPLE_NAMES="INV-1,INV-2,NS-1,NS-2,NS-3,NS-4,INV-3,INV-5,INV-6,NS-5,NS-6,INV-7,INV-8,NS-7,NS-8,NS-9,INV-10,INV-11,INV-12,NS-11,NS-12,NS-13,NS-14,INV-14,NS-15,NS-16,INV-15,INV-16,NS-17,NS-18,INV-17,INV-18,NS-19,NS-20,INV-19,INV-20,INV-21,INV-22,NS-21,NS-22,INV-23,NS-23,NS-24,INV-25,INV-26,NS-25,NS-26,NS-27,NS-28,INV-27,INV-29,INV-30,NS-29,INV-28,INV-24,NS-30,INV-4,INV-9,NS-10,INV-13";
+
+# Read 1 fastq path (',' sep). Files must appear in the same order as sample names.
+READ_1="/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/1_S13_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/2_S39_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/3_S40_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/4_S14_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/5_S54_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/6_S55_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/7_S15_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/9_S17_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/10_S18_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/11_S56_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/12_S57_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/13_S19_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/14_S41_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/15_S45_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/16_S46_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/18_S58_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/19_S42_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/21_S20_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/22_S43_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/23_S51_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/24_S52_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/25_S21_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/27_S49_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/28_S22_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/29_S23_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/32_S60_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/30_S24_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/31_S44_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/33_S61_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/34_S62_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/35_S63_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/36_S64_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/37_S65_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/38_S66_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/39_S25_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/40_S50_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/41_S67_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/43_S69_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/42_S68_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/44_S70_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/45_S26_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/46_S28_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/48_S30_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/50_S32_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/51_S33_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/49_S31_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/52_S34_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/53_S35_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/54_S36_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/DIPG002_INV_RNA_S27_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_nov2023/1_Fastq/1_S1_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_nov2023/1_Fastq/2_S2_R1_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_nov2023/1_Fastq/3_S3_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/1_S337_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/2_S338_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/3_S339_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/4_S340_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/5_S341_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/6_S342_R1_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/7_S343_R1_001.fastq.gz";
+
+# Read 2 fastq path (',' sep). Required only for paired-end analysis. Files must appear in the same order as sample names.
+READ_2="/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/1_S13_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/2_S39_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/3_S40_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/4_S14_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/5_S54_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/6_S55_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/7_S15_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/9_S17_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/10_S18_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/11_S56_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/12_S57_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/13_S19_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/14_S41_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/15_S45_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/16_S46_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/18_S58_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/19_S42_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/21_S20_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/22_S43_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/23_S51_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/24_S52_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/25_S21_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/27_S49_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/28_S22_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/29_S23_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/32_S60_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/30_S24_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/31_S44_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/33_S61_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/34_S62_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/35_S63_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/36_S64_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/37_S65_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/38_S66_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/39_S25_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/40_S50_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/41_S67_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/43_S69_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/42_S68_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/44_S70_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/45_S26_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/46_S28_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/48_S30_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/50_S32_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/51_S33_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/49_S31_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/52_S34_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/53_S35_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/54_S36_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_luglio2023/1_FASTQ/DIPG002_INV_RNA_S27_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_nov2023/1_Fastq/1_S1_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_nov2023/1_Fastq/2_S2_R2_001.fastq.gz,/home/giulio/projects/OPBG/Pericoli_nov2023/1_Fastq/3_S3_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/1_S337_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/2_S338_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/3_S339_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/4_S340_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/5_S341_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/6_S342_R2_001.fastq.gz,/home/giulio/projects/OPBG/Vinci_052024/7_S343_R2_001.fastq.gz";
+
+# Sample types (',' sep). Types must appear in the same order as sample names.
+TYPE="INV,INV,NS,NS,NS,NS,INV,INV,INV,NS,NS,INV,INV,NS,NS,NS,INV,INV,INV,NS,NS,NS,NS,INV,NS,NS,INV,INV,NS,NS,INV,INV,NS,NS,INV,INV,INV,INV,NS,NS,INV,NS,NS,INV,INV,NS,NS,NS,NS,INV,INV,INV,NS,INV,INV,NS,INV,INV,NS,INV"
+
+# Output directory.
+OUTPUT_DIR="/opt/ngs";
+
+# Reference genome file path for bowtie.
+BOWTIE_DNA="/opt/genome/human/hg19/index/bowtie2/hg19.filtered";
+
+# Reference genome file path for hisat2.
+HISAT_DNA="/opt/genome/human/hg19/index/hisat2/hg19.filtered";
+
+# Reference genome annotation file path.
+BED_FILE="/opt/genome/human/hg19/annotation/hg19.refseq.bed12";
+
+# Phix genome file path.
+PHIX_DNA="/opt/genome/control/phix174/bwa/phiX174.fa";
+
+# Ribosomal genome 1 (5S) file path.
+RIBOSOMAL_DNA_1="/opt/genome/human/hg19/contam/bwa/hum5SrDNA.fa";
+
+# Ribosomal genome 2 file path.
+RIBOSOMAL_DNA_2="/opt/genome/human/hg19/contam/bwa/humRibosomal.fa";
+
+# Max thread number.
+THREADS="12";
+
+# GTF file path.
+GFT_FILE="/opt/genome/human/hg19/annotation/hg19.refgene.sorted.gtf";
+
+# Alignment method. Options: hisat, tophat.
+ALIGNMENT="hisat";
+
+# Library type. Options: fr-firststrand, fr-secondstrand.
+LIBRARY="fr-secondstrand";
+
+# Quantification method. Options: featureCounts, Cufflinks.
+QUANTIFICATION="featureCounts";
+
+# Reference genome file path.
+REFERENCE_DNA="/opt/genome/human/hg19/index/hg19.filtered.fa";
+
+# Differential Expression Analysis method. Options: edgeR, DESeq2, cummeRbund.
+DEA="edgeR";
+
+# Script directory.
+#SCRIPT_DIR="/opt/applications/src/ARPIR";
+SCRIPT_DIR="/opt/applications/src/creo_pipelines/pipelines";
+
+# Analysis with or without final meta-analysis. Options: full, quant.
+META_ANALYSIS="full";
+
+# Max number of categories showed in R plots for meta-analysis.
+CATEGORY_NUMBER="5";
+
+# LOG file.
+#LOG="/opt/ngs/logs/simulation.log";
+LOG="/opt/ngs/logs/${TODAY}_rna_seq_opbg_${POOL_NAME}.log";
+
+#Comparisons (treat1_VS_cntrl,treat2_VS_cntrl).
+COMPARISONS="INV_VS_NS";
+
+nohup python ${SCRIPT_DIR}/RNAseq_pipeline.py -n ${PROJECT_NAME} -pn ${POOL_NAME} -sn ${SAMPLE_NAMES} -r1 ${READ_1} -r2 ${READ_2} -type ${TYPE} -o ${OUTPUT_DIR} -rb ${BOWTIE_DNA} -rh ${HISAT_DNA} -bed ${BED_FILE} -ph ${PHIX_DNA} -rib1 ${RIBOSOMAL_DNA_1} -rib2 ${RIBOSOMAL_DNA_2} -t ${THREADS} -g ${GFT_FILE} -a ${ALIGNMENT} -l ${LIBRARY} -q ${QUANTIFICATION} -r_path ${SCRIPT_DIR} -r ${REFERENCE_DNA} -dea ${DEA} -comp ${COMPARISONS} -meta ${META_ANALYSIS} -cat ${CATEGORY_NUMBER} 2>&1 > ${LOG} &
